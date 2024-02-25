0cc485ef30d2f46d39b657892c0ab28ecac8f1ad
Atharv Mishra <antilogatharv@gmail.com>
2024-02-25 18:54:30 +0530

    final commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import inquirer from 'inquirer';
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+        message: "Type in your URL: ",
+        name: "URL",
+    },
+  ])
+  .then((answers) => {
+        const url = answers.URL;
+        var qr_svg = qr.image(url);
+        qr_svg.pipe(fs.createWriteStream(`my_qr.png`));
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.error("Prompt couldn't be rendered in the current environment. Please run the script in a different environment.");
+    } else {
+      console.error("An unexpected error occurred:", error.message);
+    }
+  });
+  
